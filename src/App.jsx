387873821e1dd155032c589e69f7620b1dbc8dfd
arr1790 <anaruano1790@gmail.com>
2025-02-28 14:25:11 +0100

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Favourite from "./pages/Favourite";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/movies/:movieId" element={<MovieDetail />} />
         <Route path="/" element={<Footer />} />
+        <Route path="/favourite" element ={<Favourite />} />
       </Routes>
     </Router>
   );
